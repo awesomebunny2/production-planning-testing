6ce5e7d66e37cbdf00a89039cd1e327e03661f7e
awesomebunny2 <awesomebunny@bellsouth.net>
2024-03-26 13:27:17 -0400

    added globalVar.js to files

---
+++ globalVar.js
@@ -0,0 +1,48 @@
+﻿export default {
+    silkLayouts: [],
+    sheetHourData: {},
+    breakoutData: {},
+    productData: {},
+    wasteData: {},
+    linesData:[],
+    formToCarry: undefined,
+    changeEvent: undefined,
+    eventResult: undefined,
+    formsColumnIndex: undefined,
+    result: "",
+    scrollErr: undefined,
+    silkTable: undefined,
+    textTable: undefined,
+    digTable: undefined,
+    monday:[],
+    tuesday:[],
+    wednesday:[],
+    thursday:[],
+    friday:[],
+    silkDataSet:[],
+    textDataSet:[],
+    digDataSet:[],
+    operators: {},
+    scrollHeight: undefined,
+    listOfBreakoutTables:[],
+    emptySheets:[],
+    normalBreakoutsFormatting: {},
+    hiddenLinesData: ["MISSING", "IGNORE", "Shipping", "Empty", "PRINTED", "DIGITAL"],
+    priorityNum: 1,
+    singleSided: false,
+    dotSelected: false,
+    plannedSep: false,
+    rushItem: false,
+    opTableCounter: 1,
+    rowMoved: false,
+    pressmen:[" "],
+    presses:[" "],
+    daysOfWeek: [" ", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    //pressmen: [" ", "Steve", "Roberto", "Ryan", "Jamie", "Cody", "Terry", "Paul"],
+    //presses: [" ", 1, 2, 3, 4, "Digital 1", "Digital 2"]
+}
+
+
+
+
+       
